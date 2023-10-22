05b1d31e898881fac1b47dd43919e5532f9d7c51
msambyal <msambyal@texala.net>
2023-10-22 11:46:54 +0530

    Worked on frontEnd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Todo from "./components/Todo";
+import { addToDo, getAllToDo, updateToDo, deleteToDo } from "./utils/HandleApi";
 
 function App() {
+  const [todo, setToDo] = useState([]);
+  const [text, setText] = useState("");
+  const [toDoId, setToDoId] = useState("");
+  const [isUpdating, setIsUpdating] = useState(false);
+
+  useEffect(() => {
+    getAllToDo(setToDo);
+  }, []);
+
+  const updateMode = (_id, text) => {
+    setIsUpdating(true);
+    setToDoId(_id);
+    setText(text);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>ToDO App</h1>
+        <div className="top">
+          <input
+            type="text"
+            name="todo"
+            value={text}
+            placeholder="Add Todo..."
+            onChange={(e) => setText(e.target.value)}
+          />
+          <div
+            className="add"
+            onClick={
+              isUpdating
+                ? () =>
+                    updateToDo(toDoId, text, setText, setToDo, setIsUpdating)
+                : () => addToDo(text, setText, setToDo)
+            }
+          >
+            {isUpdating ? "Update" : "Add"}
+          </div>
+        </div>
+        <div className="list">
+          {todo.map((item) => (
+            <Todo
+              key={item._id}
+              text={item.text}
+              updateMode={() => updateMode(item._id, item.text)}
+              deleteToDo={() => deleteToDo(item._id, setToDo)}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
